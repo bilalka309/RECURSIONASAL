e82487adf4502beb1d25cff15c19762d48d41ce9
bilal karahan <bilalkarahan309@gmail.com>
2022-10-01 16:52:27 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+static void asal(int x,int y){
+    if(x==y){
+        System.out.println("Asal");
+        return;
+    }else if(x%y==0){
+        System.out.println("Asal değil.");
+        return;
+    }
+    asal(x,y++);
+}
+public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    System.out.println("Sayı giriniz : ");
+    int x= in.nextInt();
+    asal(x,2);
+
     }
 }
